56742d8a00e1a6648c09c8fc227a253a1a9d4d4b
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-08-18 20:29:54 +0200

    feat: import Player class and add function for calculate total rating

--- app/main.py
+++ app/main.py
@@ -1,1 +1,5 @@
-# write your code here
+from app.players.player import Player
+
+
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player.get_rating() for player in players)
