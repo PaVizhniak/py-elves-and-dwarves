334e6f75707b307e3ff351b47239ff337d597799
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-08-18 20:39:46 +0200

    feat: import Elf class and add elves_concert method

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from app.players.player import Player
+from app.players.elves.elf import Elf
 
 
 def calculate_team_total_rating(players: list[Player]) -> int:
     return sum(player.get_rating() for player in players)
+
+def elves_concert(elves: list[Elf]) -> None:
+    for elf in elves:
+        print(elf.play_elf_song())
