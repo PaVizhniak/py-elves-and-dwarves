b89e2ca629dd306c4db6910ffa33b67744dcd51e
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-08-18 19:11:05 +0200

    feat: create dwarves package with Dwarf class

---
+++ app/players/dwarves/dwarf.py
@@ -0,0 +1,16 @@
+from ..player import Player
+
+
+class Dwarf(Player):
+    def __init__(self, nickname: str, favourite_dish: str) -> None:
+        super().__init__(nickname)
+        self._favourite_dish = favourite_dish
+
+    def get_rating(self) -> None:
+        pass
+
+    def player_info(self) -> None:
+        pass
+
+    def eat_favourite_dish(self) -> str:
+        return f"{self.nickname} is eating {self._favourite_dish}"
