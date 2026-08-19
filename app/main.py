0d41ab1bc8f24b9bfaef2b28955276f23a4f63b7
Pavlo_Vizhniak <pavlovizhniak@gmail.com>
2026-08-19 17:54:21 +0200

    fix: errors in import and in variable names

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
-from app.players import player
-from app.players.elves import elf, elf_ranger, druid
-from app.players.dwarves import dwarf, dwarf_warrior, dwarf_blacksmith
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
 
-def calculate_team_total_rating(players: list[player.Player]) -> int:
-    return sum(player.get_rating() for player in players)
+def calculate_team_total_rating(players: list[Player]) -> int:
+    return sum(player_member.get_rating() for player_member in players)
 
 
-def elves_concert(elves: list[elf.Elf]) -> None:
-    for elf in elves:
-        elf.play_elf_song()
+def elves_concert(elves: list[Elf]) -> None:
+    for elf_member in elves:
+        elf_member.play_elf_song()
 
 
-def feast_of_the_dwarves(dwarfs: list[dwarf.Dwarf]) -> None:
-    for dwarf in dwarfs:
-        dwarf.eat_favourite_dish()
+def feast_of_the_dwarves(dwarfs: list[Dwarf]) -> None:
+    for dwarf_member in dwarfs:
+        dwarf_member.eat_favourite_dish()
